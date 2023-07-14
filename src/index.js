d897fefa151195c5190c760e6e9cf6c6abb02b52
Evnomad <b.boy.bip@gmail.com>
2023-07-14 23:15:15 +0700

    list icons

--- src/index.js
+++ src/index.js
@@ -3,12 +3,36 @@ const $ = require('jquery');
 window.jQuery = $;
 window.$ = $;
 
+import SimpleBar from 'simplebar';
+
 import './scss/main.scss';
 import './js/assets';
 
 (function () {
-  window.addEventListener('DOMContentLoaded', (event) => {
-
+  window.addEventListener('DOMContentLoaded', () => {
+    activateBlockScroll('.explore-categories', '.explore-categories-cards');
   });
 
 })();
+
+const activateBlockScroll = (block, scrollWrapper) => {
+  const element = $(block);
+
+  if (!element.length) return;
+
+  initSimpleBar(element.find(scrollWrapper));
+};
+
+
+function initSimpleBar(element) {
+  $(element).each(function () {
+    const current = $(this).get(0);
+    const simpleBarElement = new SimpleBar(current, {
+      autoHide: false,
+      forceVisible: true
+    });
+    if (element.hasClass('listing-cards')) {
+      listingScrollEvent(simpleBarElement);
+    }
+  });
+}
